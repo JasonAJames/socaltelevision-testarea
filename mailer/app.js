0bc775ae1a5ef3b62e62a75e25232cb99b38f82b
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-04-18 12:46:40 -0700

    moved node express mailer app in src to root directory and started server hopefully

--- mailer/app.js
+++ mailer/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+var server = require('http').Server(app);
 const nodemailer = require('nodemailer');
 
 const app = express();
@@ -9,4 +10,6 @@ app.get('/',(req, res) => {
     res.send('Hello from ROOT mailer');
 });
 
-app.listen(8080, () => console.log('Server Started Correctly!'));
+var listener = server.listen(process.env.PORT || 8080, function(){
+    console.log('Listening on port ' + listener.address().port); //Listening on port 5000
+});
