452a0ec83ac8efccc02e4c62a22abe8764c2b502
SoCal Television <socaltelevision@SoCals-MacBook-Pro.local>
2019-04-26 20:12:34 -0700

    modified amazon product share meta data

--- src/app/amazon-product-reviews/amazon-product-reviews.component.ts
+++ src/app/amazon-product-reviews/amazon-product-reviews.component.ts
@@ -25,7 +25,7 @@ export class AmazonProductReviewsComponent implements OnInit {
     
   }
 
-  pagetitle = "Amazon Products"
+  pagetitle = "Search Amazon Products"
   subtitle0 = "Electronics & Home Accessories"
   subtitle1 = "Computers, Tablets & Peripherals"
   subtitle2 = "Cameras & Photography Equipment"
@@ -33,14 +33,14 @@ export class AmazonProductReviewsComponent implements OnInit {
 
   // Social Sharing binding data
   title = "SoCal Television"
-  header_link = "Special Spots"
-  url = "https://www.socaltelevision.com/+#+/amazon-product-reviews"
-  description = 'Special Spots';
-  subdescription = 'We venture throughtout Southern California and beyond, to showcase attractions, events, entertainment, places of interest, restaurant reviews, and more...'
-  summary = 'We venture throughtout Southern California and beyond, to showcase attractions, events, entertainment, places of interest, restaurant reviews, and more...'
+  header_link = "Featured Amazon Products"
+  url = "https://www.socaltelevision.com/#/amazon-product-reviews"
+  description = 'Featured Amazon Products';
+  subdescription = 'We venture throughtout Southern California and beyond, to showcase awesome Amazon products...'
+  summary = 'We venture throughtout Southern California and beyond, to showcase Amazon products, attractions, events, entertainment, places of interest, restaurant reviews, and more...'
   source_url = this.url
-  subject = "Check Out this SoCalTelevision.com website"
-  body = 'SoCalTelevision.com Special Spots ventures throughtout Southern California and beyond, to showcase attractions, events, entertainment, places of interest, restaurant reviews, and more...'
+  subject = "Check Out these SoCalTelevision.com featured Amazon products"
+  body = 'SoCalTelevision.com Special Spots ventures throughtout Southern California and beyond, to showcase Amazon products, attractions, events, entertainment, places of interest, restaurant reviews, and more...'
 
 
 }
